00958645246b6b31d7f02803495988a5452a76c0
jbok4 <jbok4@aol.com>
2016-10-06 15:39:38 -0400

    fixed sign out route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   resources :reviews
   devise_for :users
   resources :books
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   root 'books#index'
 
 end
