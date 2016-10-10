80a37b36704b2b8a45dc994284248dd665f98208
jbok4 <jbok4@aol.com>
2016-10-10 15:53:43 -0400

    validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,9 @@ class Book < ActiveRecord::Base
   belongs_to :user
   has_many :reviews
 
+  validates_uniqueness_of :title
+  validates :author, :description, :pages, :published
+
   has_attached_file :image, styles: { medium: "300x500#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :book
+
+  validates :comment, presence: true
+
 end
