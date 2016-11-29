35289404d49a2d77d69186433d4e51c92f82a9ed
jbok4 <jbok4@aol.com>
2016-11-29 16:27:11 -0500

    user serializer created and to_json added to controller show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @reviews = Review.where(user_id: @user.id).order("created_at DESC")
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @user}
+    end
   end
 
 end
