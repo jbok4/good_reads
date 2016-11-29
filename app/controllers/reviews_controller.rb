0a6c8dcc1125f896fd0284cb640857612d794887
jbok4 <jbok4@aol.com>
2016-11-29 17:10:30 -0500

    js to make next button show next user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,11 +12,11 @@ class ReviewsController < ApplicationController
   end
 
   def show
-    # @review = Review.find(params[:id])
-    # respond_to do |f|
-    #   f.html { render :show }
-    #   f.json { render json: @user }
-    # end
+    @review = Review.find(params[:id])
+    respond_to do |f|
+      f.html { render :show }
+      f.json { render json: @review }
+    end
   end
 
   def edit
