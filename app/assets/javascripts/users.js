42953bb616e7bb126df74cfe79bbb2586b98ba67
jbok4 <jbok4@aol.com>
2016-11-30 18:28:09 -0500

    .get method re-code

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,14 +1,26 @@
+// $(function () {
+// $("a.userReviews").on("click", function(e){
+//   // ajax
+//   $.ajax({
+//     method: "GET",
+//     url: this.href
+//     //get a response
+//   }).success(function(data) {
+//       //load response into the page
+//     $("#inject").html(data)
+//   });
+
+//   e.preventDefault();
+// })
+// });
+
+
 $(function () {
 $("a.userReviews").on("click", function(e){
-  // ajax
-  $.ajax({
-    method: "GET",
-    url: this.href
-    //get a response
-  }).success(function(data) {
-      //load response into the page
-    $("#inject").html(data)
-  });
+
+$.get(this.href).success(function(data){
+  $("#inject").html(data)
+})
 
   e.preventDefault();
 })
