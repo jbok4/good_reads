ca337e4ea76bc975411369ae65540db519ac495e
jbok4 <jbok4@aol.com>
2016-12-05 20:37:25 -0500

    cleaning up and making concise non-working book-review js load of form

--- app/serializers/book_review_serializer.rb
+++ app/serializers/book_review_serializer.rb
@@ -1,3 +1,3 @@
 class BookReviewSerializer < ActiveModel::Serializer
-  attributes :id, :title, :comment, :user_id, :created_at
+  attributes :id, :title, :comment, :user_id, :created_at, :rating
 end
