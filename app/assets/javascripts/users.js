ea0ad72cb7ac09b2e4ad0274e9a6f2b0d67d5a6f
jbok4 <jbok4@aol.com>
2016-12-06 21:49:58 -0500

    working on user showing all reviews link via js

--- app/assets/javascripts/users.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  $('.all-reviews').on('click', function(){
-    $('#inject-all').show();
-  });
-});
-
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,18 +20,3 @@ class UsersController < ApplicationController
   end
 
 end
-
-# devise class helper methods
-# Generated methods:
-#   authenticate_user!  # Signs user in or redirect
-#   authenticate_admin! # Signs admin in or redirect
-#   user_signed_in?     # Checks whether there is a user signed in or not
-#   admin_signed_in?    # Checks whether there is an admin signed in or not
-#   current_user        # Current signed in user
-#   current_admin       # Current signed in admin
-#   user_session        # Session data available only to the user scope
-#   admin_session       # Session data available only to the admin scope
-
-# Use:
-#   before_action :authenticate_user!  # Tell devise to use :user map
-#   before_action :authenticate_admin! # Tell devise to use :admin map
