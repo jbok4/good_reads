b586ad83e07037cf9ae35cbe203e4bee03376c2e
jbok4 <jbok4@aol.com>
2016-12-07 13:46:24 -0500

    load all reviews .each function setup

--- app/serializers/user_review_serializer.rb
+++ app/serializers/user_review_serializer.rb
@@ -1,5 +1,6 @@
 class UserReviewSerializer < ActiveModel::Serializer
-  attributes :id, :comment, :book_id, :rating, :user_id, :created_at
+  attributes :comment,  :created_at
+  # attributes :id, :comment, :book_id, :rating, :user_id, :created_at
   #problem is i am not loading this up with the book.title 
   #in the json info
   #which is a has_many through issue. 
